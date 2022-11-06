e7d2d3cdd557d483230368cd5a8f6bc32c77ef75
Zeyad Mohammed AboSehly <92084044+Zeyad-1999@users.noreply.github.com>
2022-11-06 13:25:46 +0200

    Create task 3.cpp

---
+++ task 3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BankCustomer {
+    private:
+	string name;
+	string address;
+	string mobile;
+	string birth_of_date;
+	public:
+	    get_data()// return name,address,mobile and birth_of_date
+	    set_data()// enter customer data
+};
+
+int main() {
+  BankCustomer x;
+  x.set_data();
+  x.get_data();
+
+	return 0;
+}
