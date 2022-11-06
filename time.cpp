062c8581697da449c669f3bc3cd635b0c549c40b
Zeyad Mohammed AboSehly <92084044+Zeyad-1999@users.noreply.github.com>
2022-11-06 15:07:49 +0200

    Create time.cpp

---
+++ time.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+struct time
+{
+    int hour;
+    int minuit;
+    int second;
+    bool is_hours(int hour);    
+    bool is_minuit(int miniut);
+    bool is_second(int second); 
+    string displaydata(int hour, int minuit , int second);
+
+};
+int main()
+{
+
+}
