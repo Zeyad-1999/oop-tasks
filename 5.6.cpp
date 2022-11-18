4853e9cd445fe045d3e6149f82ee4af4281b42df
Zeyad Mohammed AboSehly <92084044+Zeyad-1999@users.noreply.github.com>
2022-11-18 10:46:31 +0200

    Create 5.6.cpp

---
+++ 5.6.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Time {
+private:
+	int hours, minutes, seconds;
+
+public:
+	Time(int hours, int minutes, int seconds) :
+			hours(hours+(minutes/60)%24), minutes((minutes+(seconds/60))%60), seconds(seconds%60) {
+	}
+	void SetTime(int hours, int minutes, int seconds) {
+
+		this->hours = hours;
+		this->minutes = minutes;
+		this->seconds = seconds;
+	}
+	int GetTotalSeconds() {
+		return hours * 60 * 60 + minutes * 60 + seconds;
+	}
+	int GetTotalMinutes() {
+		return hours * 60 + minutes;
+	}
+	void PrintHHMMSS() {
+		cout << hours << ":" << minutes << ":" << seconds << "\n";
+	}
+	string ToSring(string seperator = "-") {
+		ostringstream oss;
+		oss << hours << seperator << minutes << seperator << seconds;
+		return oss.str();
+	}
+	int GetHours() {
+		return hours;
+	}
+	Time& SetHours(int hours) {
+		this->hours = hours;
+	}
+	int GetMinutes() {
+		return minutes;
+	}
+	Time& SetMinutes(int minutes) {
+		if (minutes < 0)
+			minutes = 0;
+		this->minutes = minutes;
+	}
+	int GetSeconds() {
+		return seconds;
+	}
+	Time& SetSeconds(int seconds) {
+		if (seconds < 0)
+			seconds = 0;
+		this->seconds = seconds;
+	}
+};
+
+int main() {
+	Time t(0, 120, 120);
+	t.PrintHHMMSS();
+	t.SetHours(9).SetMinutes(44).SetSeconds(5);
+		t.PrintHHMMSS();
+	return 0;
+}
